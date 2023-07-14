6231a48f33ce76c8fdd2487a01c9ce2977a2d18b
Ben Rich <beri9208@colorado.edu>
2023-07-14 13:14:16 -0600

    Update syntax

--- neb.py
+++ neb.py
@@ -204,7 +204,8 @@ if __name__ == '__main__':
             CONTCAR = os.path.join(_dir, "CONTCAR")
             POSCAR = os.path.join(_dir, "POSCAR")
             if os.path.exists(CONTCAR):
-                log_stuff(f'Image {i}: {str(CONTCAR)}')
+                msg = f'Image {i}: {str(CONTCAR)}'
+                log_stuff(msg)
                 images.append(read(CONTCAR))
                 images_og_dirs.append(CONTCAR)
             elif os.path.exists(POSCAR):
