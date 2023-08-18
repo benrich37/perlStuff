dd748a4bc25ee27ff76c3a0bf073c28f2309f540
Ben Rich <beri9208@colorado.edu>
2023-08-18 13:20:06 -0600

    Some variables were silently being called as global variables, didnt realize until I put into a main() function

--- opt.py
+++ opt.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from helpers.generic_helpers import get_cmds, get_inputs_list, fix_work_dir, optimizer, remove_dir_recursive, \
     get_atoms_list_from_out, get_do_cell
 from helpers.generic_helpers import _write_contcar, get_log_fn, dump_template_input, read_pbc_val
-from helpers.calc_helpers import _get_calc, get_exe_cmd, set_calc_old
+from helpers.calc_helpers import _get_calc, get_exe_cmd
 from helpers.generic_helpers import check_submit, get_atoms_from_coords_out
 from helpers.generic_helpers import copy_best_state_files, has_coords_out_files, get_lattice_cmds, get_ionic_opt_cmds
 from helpers.generic_helpers import _write_opt_log, check_for_restart, log_def, check_structure, log_and_abort
@@ -87,7 +87,7 @@ def get_atoms_from_lat_dir(dir):
     return get_atoms_from_coords_out(ionpos, lattice)
 
 
-def get_restart_structure(structure, restart, opt_dir, lat_dir, use_jdft, log_fn=log_def):
+def get_restart_structure(structure, restart, work_dir, opt_dir, lat_dir, use_jdft, log_fn=log_def):
     if ope(opt_dir):
         if not use_jdft:
             if ope(opj(opt_dir, "CONTCAR")):
@@ -130,7 +130,7 @@ def get_restart_structure(structure, restart, opt_dir, lat_dir, use_jdft, log_fn
     return structure, restart
 
 
-def get_structure(structure, restart, opt_dir, lat_dir, lat_iters, use_jdft, log_fn=log_def):
+def get_structure(structure, restart, work_dir, opt_dir, lat_dir, lat_iters, use_jdft, log_fn=log_def):
     dirs_list = [opt_dir]
     if lat_iters > 0:
         log_fn(f"Lattice opt requested ({lat_iters} iterations) - adding lat dir to setup list")
@@ -146,16 +146,13 @@ def get_structure(structure, restart, opt_dir, lat_dir, lat_iters, use_jdft, log
         else:
             log_and_abort(f"Requested structure {structure} not found", log_fn=log_fn)
     else:
-        structure, restart = get_restart_structure(structure, restart, opt_dir, lat_dir, use_jdft, log_fn=log_fn)
+        structure, restart = get_restart_structure(structure, restart, work_dir, opt_dir, lat_dir, use_jdft, log_fn=log_fn)
     return structure, restart
 
 
-def run_lat_opt_runner(atoms, structure, lat_iters, lat_dir, root, log_fn, cmds):
-    lat_cmds = get_lattice_cmds(cmds, lat_iters, atoms.pbc)
-    get_lat_calc = lambda root: _get_calc(exe_cmd, lat_cmds, root, log_fn=log_fn)
-    atoms.set_calculator(get_lat_calc(lat_dir))
+def run_lat_opt_runner(atoms, structure, lat_dir, root, calc_fn, log_fn=log_def):
+    atoms.set_calculator(calc_fn(lat_dir))
     log_fn("lattice optimization starting")
-    log_fn(f"Fmax: n/a, max_steps: {lat_iters}")
     atoms.get_forces()
     ionpos = opj(lat_dir, "ionpos")
     lattice = opj(lat_dir, "lattice")
@@ -164,30 +161,28 @@ def run_lat_opt_runner(atoms, structure, lat_iters, lat_dir, root, log_fn, cmds)
     atoms.pbc = pbc
     structure = opj(root, structure + "_lat_opted")
     write(structure, atoms, format="vasp")
-    opt_log(f"Finished lattice optimization")
+    log_fn(f"Finished lattice optimization")
     finished(lat_dir)
     out_to_logx(lat_dir, opj(lat_dir, 'out'), log_fn=log_fn)
     return atoms, structure
 
 
-def run_lat_opt(atoms, structure, lat_iters, lat_dir, root, log_fn, cmds, _failed_before=False):
+def run_lat_opt(atoms, structure, lat_dir, root, calc_fn, log_fn=log_def, _failed_before=False):
     run_again = False
     try:
-        atoms, structure = run_lat_opt_runner(atoms, structure, lat_iters, lat_dir, root, log_fn, cmds)
+        atoms, structure = run_lat_opt_runner(atoms, structure, lat_dir, root, calc_fn, log_fn=log_fn)
     except Exception as e:
         check_for_restart(e, _failed_before, lat_dir, log_fn=log_fn)
         run_again = True
         pass
     if run_again:
-        atoms, structure = run_lat_opt(atoms, structure, lat_iters, lat_dir, root, log_fn, cmds, _failed_before=True)
+        atoms, structure = run_lat_opt(atoms, structure, lat_dir, root, calc_fn, log_fn=log_fn, _failed_before=True)
     return atoms, structure
 
 
-def run_ion_opt_runner(atoms_obj, ion_iters_int, ion_dir_path, cmds_list, log_fn=log_def):
-    ion_cmds = get_ionic_opt_cmds(cmds_list, ion_iters_int)
-    atoms_obj.set_calculator(_get_calc(exe_cmd, ion_cmds, ion_dir_path, log_fn=log_fn))
+def run_ion_opt_runner(atoms_obj, ion_dir_path, calc_fn, log_fn=log_def):
+    atoms_obj.set_calculator(calc_fn(ion_dir_path))
     log_fn("ionic optimization starting")
-    log_fn(f"Fmax: n/a, max_steps: {ion_iters_int}")
     pbc = atoms_obj.pbc
     atoms_obj.get_forces()
     outfile = opj(ion_dir_path, "out")
@@ -199,26 +194,26 @@ def run_ion_opt_runner(atoms_obj, ion_iters_int, ion_dir_path, cmds_list, log_fn
     atoms_obj.pbc = pbc
     structure_path = opj(ion_dir_path, "CONTCAR")
     write(structure_path, atoms_obj, format="vasp")
-    log_fn(f"Finished ionic optimization in {len(atoms_obj_list) - 1}/{ion_iters_int}")
     finished(ion_dir_path)
     out_to_logx(ion_dir_path, opj(ion_dir_path, 'out'), log_fn=log_fn)
     return atoms_obj
 
 
-def run_ion_opt(atoms_obj, ion_iters_int, ion_dir_path, root_path, cmds_list, _failed_before=False, log_fn=log_def):
+def run_ion_opt(atoms_obj, ion_dir_path, root_path, calc_fn, _failed_before=False, log_fn=log_def):
     run_again = False
     try:
-        atoms_obj = run_ion_opt_runner(atoms_obj, ion_iters_int, ion_dir_path, cmds_list, log_fn=log_fn)
+        atoms_obj = run_ion_opt_runner(atoms_obj, ion_dir_path, calc_fn, log_fn=log_fn)
     except Exception as e:
         check_for_restart(e, _failed_before, ion_dir_path, log_fn=log_fn)
         run_again = True
         pass
     if run_again:
-        atoms_obj = run_ion_opt(atoms_obj, ion_iters_int, ion_dir_path, root_path, cmds_list, _failed_before=True, log_fn=log_fn)
+        atoms_obj = run_ion_opt(atoms_obj, ion_dir_path, root_path, calc_fn, _failed_before=False, log_fn=log_fn)
     return atoms_obj
 
 
-def run_ase_opt_runner(atoms, root, opter, do_cell, fmax, max_steps, log_fn=log_def):
+def run_ase_opt_runner(atoms, root, opter, fmax, max_steps, log_fn=log_def):
+    do_cell = get_do_cell(atoms.pbc)
     dyn = optimizer(atoms, root, opter)
     traj = Trajectory(opj(root, "opt.traj"), 'w', atoms, properties=['energy', 'forces', 'charges'])
     logx = opj(root, "opt.logx")
@@ -238,18 +233,17 @@ def run_ase_opt_runner(atoms, root, opter, do_cell, fmax, max_steps, log_fn=log_
     finished(root)
 
 
-def run_ase_opt(atoms, opt_dir, opter, cell_bool, exe_cmd, cmds, fmax, max_steps, log_fn=log_def, _failed_before=False):
-    get_calc = lambda root: _get_calc(exe_cmd, cmds, root, log_fn=log_fn)
-    atoms.set_calculator(get_calc(opt_dir))
+def run_ase_opt(atoms, opt_dir, opter, calc_fn, fmax, max_steps, log_fn=log_def, _failed_before=False):
+    atoms.set_calculator(calc_fn(opt_dir))
     run_again = False
     try:
-        run_ase_opt_runner(atoms, opt_dir, opter, cell_bool, fmax, max_steps, log_fn=log_fn)
+        run_ase_opt_runner(atoms, opt_dir, opter, fmax, max_steps, log_fn=log_fn)
     except Exception as e:
         check_for_restart(e, _failed_before, opt_dir, log_fn=log_fn)
         run_again = True
         pass
     if run_again:
-        run_ase_opt(atoms, opt_dir, opter, cell_bool, exe_cmd, cmds, fmax, max_steps, log_fn=log_fn, _failed_before=True)
+        run_ase_opt(atoms, opt_dir, opter, calc_fn, fmax, max_steps, log_fn=log_fn, _failed_before=True)
 
 def main():
     work_dir, structure, fmax, max_steps, gpu, restart, pbc, lat_iters, use_jdft = read_opt_inputs()
@@ -259,24 +253,27 @@ def main():
     structure = opj(work_dir, structure)
     opt_log = get_log_fn(work_dir, "opt", False, restart=restart)
     structure = check_structure(structure, work_dir, log_fn=opt_log)
-    structure, restart = get_structure(structure, restart, opt_dir, lat_dir, lat_iters, use_jdft)
+    structure, restart = get_structure(structure, restart, work_dir, opt_dir, lat_dir, lat_iters, use_jdft)
     exe_cmd = get_exe_cmd(gpu, opt_log)
     cmds = get_cmds(work_dir, ref_struct=structure)
+    lat_cmds = get_lattice_cmds(cmds, lat_iters, pbc)
+    ion_cmds = get_ionic_opt_cmds(cmds, max_steps)
+    get_calc = lambda root: _get_calc(exe_cmd, cmds, root, log_fn=opt_log)
+    get_lat_calc = lambda root: _get_calc(exe_cmd, lat_cmds, root, log_fn=opt_log)
+    get_ion_calc = lambda root: _get_calc(exe_cmd, ion_cmds, root, log_fn=opt_log)
     opt_log(f"Setting {structure} to atoms object")
     atoms = read(structure, format="vasp")
-    do_cell = get_do_cell(pbc)
-    atoms.pbc = pbc
     check_submit(gpu, os.getcwd(), "opt", log_fn=opt_log)
     if (lat_iters > 0) and (not ope(opj(lat_dir, "finished.txt"))):
-        atoms, structure = run_lat_opt(atoms, structure, lat_iters, lat_dir, work_dir, opt_log, cmds)
+        atoms, structure = run_lat_opt(atoms, structure, lat_dir, work_dir, get_lat_calc, log_fn=opt_log)
     opt_log(f"Finding/copying any state files to {opt_dir}")
     copy_best_state_files([work_dir, lat_dir], opt_dir, log_fn=opt_log)
     if use_jdft:
         opt_log(f"Running ion optimization with JDFTx optimizer")
-        run_ion_opt(atoms, max_steps, opt_dir, work_dir, cmds, log_fn=opt_log)
+        run_ion_opt(atoms, opt_dir, work_dir, get_ion_calc, log_fn=opt_log)
     else:
         opt_log(f"Running ion optimization with ASE optimizer")
-        run_ase_opt(atoms, opt_dir, FIRE, do_cell, exe_cmd, cmds, fmax, max_steps, log_fn=log_def)
+        run_ase_opt(atoms, opt_dir, FIRE, get_calc, fmax, max_steps, log_fn=log_def)
 
 
 
