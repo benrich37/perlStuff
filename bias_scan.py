320530692c0a34a2c528d8c1e4aa517a6955629d
Ben Rich <beri9208@colorado.edu>
2024-03-18 16:13:07 -0600

    debug

--- bias_scan.py
+++ bias_scan.py
@@ -428,6 +428,8 @@ def _scan_step_runner(step_dir, ref_dir, fmax, max_steps, pbc, lat_iters, pseudo
     atoms = get_atoms_from_out(opj(ref_dir_calc_dir, "out"))
     atoms.pbc = pbc
     log_fn(f"Ref atoms: {atoms}")
+    log_fn(f"Ref atoms cell: {atoms.cell}")
+    write(opj(step_dir, "POSCAR.gjf"), atoms, format="gaussian-in")
     ion_dir = define_dir(step_dir, "ion_opt")
     if not is_finished(ion_dir):
         if lat_iters > 0:
