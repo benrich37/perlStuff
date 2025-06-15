4a17d9cc1600208f35c54e778ce544c7479a8cee
benrich37 <benjamin.rich@colorado.edu>
2025-06-14 18:10:20 -0600

    forgot to fix here

--- helpers/generic_helpers.py
+++ helpers/generic_helpers.py
@@ -721,10 +721,19 @@ def get_apply_freeze_func(freeze_base, freeze_tol, freeze_count, freeze_idcs, ex
     return apply_freeze_func
 
 def add_freeze_surf_base_constraint(atoms, freeze_base = False, ztol = 1.0, freeze_count = 0, exclude_freeze_count=0, log_fn=log_def, freeze_idcs=None):
-    log_fn(f"add_freeze_surf_base_constraint: {freeze_idcs}")
-    if freeze_base:
-        c = get_freeze_surf_base_constraint(atoms, ztol=ztol, freeze_count = freeze_count, exclude_freeze_count=exclude_freeze_count, log_fn=log_fn, freeze_idcs=freeze_idcs)
+    if freeze_base or len(freeze_idcs):
+        c = get_freeze_surf_base_constraint(
+            atoms,
+            ztol=ztol, freeze_count=freeze_count, freeze_idcs=freeze_idcs, exclude_freeze_count=exclude_freeze_count,
+            log_fn=log_fn)
         add_constraint(atoms, c)
+        return atoms
+    else:
+        return atoms
+    # log_fn(f"add_freeze_surf_base_constraint: {freeze_idcs}")
+    # if freeze_base:
+    #     c = get_freeze_surf_base_constraint(atoms, ztol=ztol, freeze_count = freeze_count, exclude_freeze_count=exclude_freeze_count, log_fn=log_fn, freeze_idcs=freeze_idcs)
+    #     add_constraint(atoms, c)
 
 
 def add_bond_constraints(atoms, indices, log_fn=log_def):
